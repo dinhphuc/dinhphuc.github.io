41e77a97bc8c6f5ea00126e30288f829517d76da
phucnd <minhphucpk.1997@gmail.com>
2021-12-15 11:30:34 +0700

    Updated: count day

---
+++ house/js/functions.js
@@ -0,0 +1,14 @@
+﻿
+$(document).ready(function () {
+    $('input').keypress(function (e) {
+        if (e.which === 13) {
+            e.preventDefault();
+            login();
+            return false;
+        }
+    });
+    $('#btnLogin').click(function (e) {
+        e.preventDefault();
+        login();
+    });
+});
